8e5d19e21f542dee277d2a522cdb45d66b19ba5d
Jennifer Cole <capauldi@gmail.com>
2022-03-22 16:41:35 -0700

    graphql added using apollo server

--- server/server.js
+++ server/server.js
@@ -1,8 +1,34 @@
 const express = require('express');
+
+// Import ApolloServer
+const { ApolloServer } = require('apollo-server-express');
+// Import our typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas');
+
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+const startServer = async () => {
+  // Create a new Apollo server and pass in our schema data
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers
+  });
+
+  // Start the Apollo server
+  await server.start();
+
+  // Integrate our Apollo server with the Express application as middleware
+  server.applyMiddleware({ app });
+
+  // Log where we can go to test our GQL API
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+}
+
+// Initialize the Apollo server
+startServer();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
